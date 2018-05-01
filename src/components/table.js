b90c8eb7c707ff75e3b77e233a076256db80aa5b
aurorabora <borabora.choi@gmail.com>
2018-04-30 19:34:28 -0700

    finished MVP for student grade table

--- src/components/table.js
+++ src/components/table.js
@@ -1,45 +1,30 @@
 import React from 'react';
 
 
-function Table (props) {
-    return(
-        <table className= "table">
-            <thead className= "thead-inverse">
-                <tr>
-                    <th>Name</th>
-                    <th>Course</th>
-                    <th>Grade</th>
-                </tr>
+function Table(props) {
+    const tableRows = props.data.map((item, index) => {
+        return (
+            <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.course}</td>
+                <td>{item.grade}</td>
+            </tr>
+        )
+    });
+
+    return (
+        <table className="table">
+            <thead className="thead-inverse">
+                {tableRows}
             </thead>
-            <tbody>
-                <tr>
-                    <td>Canine</td>
-                    <td>Best Friend</td>
-                    <td>9000</td>
-                </tr>
-                <tr>
-                    <td>Feline</td>
-                    <td>Evil murderers</td>
-                    <td>95555</td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr>
-                        <td>Birds</td>
-                        <td>Squeakums</td>
-                        <td>1000</td>
-                    </tr>
-                </tbody>
         </table>
     )
 }
 
-const tableRows=props.data.map((item, index) => {
-console.log(item);
-return;
+// const tableRows = props.data.map((item, index) => {
+// console.log(item);
+// return;
 
-});
+// });
 
 export default Table;
-
-
