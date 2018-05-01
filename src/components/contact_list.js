a7f1b2ca60c48710df743644bb5132f25ac424e6
aurorabora <borabora.choi@gmail.com>
2018-05-01 14:53:49 -0700

    reworked on contacts list because some data was missing after last save

--- src/components/contact_list.js
+++ src/components/contact_list.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react';
 
 // function ContactList extends Component 
 
-
+class ContactList extends Component {
+    render(){
+        return <h1>hello</h1>
+    }
+}
 
 
 export default ContactList;
---
+++ src/components/contacts.js
@@ -0,0 +1,22 @@
+export default [];
+
+export default [
+    {
+        firstName:'sdfsdf',
+        lastName:'dfgdfg',
+        phone:'wefwe',
+        email:'fhgfgh'
+    },
+    {
+        firstName:'fghf',
+        lastName:'htrt',
+        phone:'eregr',
+        email:'grerg'
+    },
+    {
+        firstName:'twewr',
+        lastName:'yjyj',
+        phone:'wetrt',
+        email:'htrhwer'
+    }
+]
